9a2d1b1ce855a4236a9b992634eb36daee9bc5d7
shyam <shyamchatarkar284@gmail.com>
2026-02-24 08:56:16 +0530

    initial server setup

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+mongoose.connect(process.env.MONGO_URL)
+
+app.use("/", (req, res) => {
+    res.status(200).json({ message: "task manager Api Runing" })
+})
+
+mongoose.connection.once("open", () => {
+    console.log("db connect");
+    app.listen(process.env.PORT, () => {
+        console.log("server runing");
+        console.log(`mode: ${process.env.NODE_ENV}`);
+    })
+
+})
+module.exports = app 
